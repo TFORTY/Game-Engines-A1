856a4c6a48d595ad6bf1f0c12ae3b20579ef1f5a
Tyler <tyler.wong@ontariotechu.net>
2021-09-29 18:07:13 -0400

    Added a material lerp script

---
+++ Assets/[Scripts]/MaterialLERP.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MaterialLERP : MonoBehaviour
+{
+    public Material mat1;
+    public Material mat2;
+    public float duration = 2f;
+    private Renderer rend;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rend = GetComponent<Renderer>();
+
+        rend.material = mat1;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float lerp = Mathf.PingPong(Time.time, duration) / duration;
+        rend.material.Lerp(mat1, mat2, lerp);
+    }
+}
