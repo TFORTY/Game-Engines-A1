70d7d89ecf63373b442df49b6e3821582b9886fa
Tyler <tyler.wong@ontariotechu.net>
2021-09-29 14:17:41 -0400

    Destroyed planets when past screen bounds

--- Assets/[Scripts]/Planet.cs
+++ Assets/[Scripts]/Planet.cs
@@ -8,18 +8,28 @@ public class Planet : MonoBehaviour
     public float ySpeed = 0f;
     public float zSpeed = 0f;
     public float moveSpeed = 10f;
+    private float screenOffset = 50f;
 
     private Rigidbody rb;
+    private Vector3 screenBounds;
 
     private void Start()
     {
         rb = GetComponent<Rigidbody>();
         rb.velocity = new Vector3(-moveSpeed, 0);
+        screenBounds = Camera.main.ScreenToWorldPoint(new Vector3(Screen.width, Screen.height, Camera.main.transform.position.z));
     }
 
     // Update is called once per frame
     void Update()
     {
+        // Rotate the planets on their respective axis
         transform.Rotate( xSpeed * Time.deltaTime, ySpeed * Time.deltaTime, zSpeed * Time.deltaTime);
+
+        // Removes game object if passed screen bounds
+        if (transform.position.x < screenBounds.x - screenOffset)
+        {
+            Destroy(this.gameObject);
+        }
     }
 }
