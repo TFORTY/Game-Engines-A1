558f952504f300ffafaacda3f3f7294b1056b838
Tyler <tyler.wong@ontariotechu.net>
2021-09-29 13:18:05 -0400

    Made planets move and rotate

--- Assets/[Scripts]/Planet.cs
+++ Assets/[Scripts]/Planet.cs
@@ -2,11 +2,20 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Rotater : MonoBehaviour
+public class Planet : MonoBehaviour
 {
     public float xSpeed = 0f;
     public float ySpeed = 0f;
     public float zSpeed = 0f;
+    public float moveSpeed = 10f;
+
+    private Rigidbody rb;
+
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        rb.velocity = new Vector3(-moveSpeed, 0);
+    }
 
     // Update is called once per frame
     void Update()
