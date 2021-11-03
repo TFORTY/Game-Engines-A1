5c67961e88e0a2e5ba9f68b6906bfb95eb50375f
Tyler <tyler.wong@ontariotechu.net>
2021-11-03 15:23:07 -0400

    Removed test scene

--- Assets/[Scripts]/Player.cs
+++ Assets/[Scripts]/Player.cs
@@ -52,8 +52,6 @@ public class Player : MonoBehaviour
     public Material blueMat;
     public Material yellowMat;
 
-    Vector3 playerStartPos;
-
     // Start is called before the first frame update
     void Start()
     {
