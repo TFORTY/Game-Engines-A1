7081f59b0c469904528d9c0778d1b5314dda0af4
Tyler <tyler.wong@ontariotechu.net>
2021-11-03 20:33:30 -0400

    Implemented simple tutorial

--- Assets/[Scripts]/TutorialLogic.cs
+++ Assets/[Scripts]/TutorialLogic.cs
@@ -2,12 +2,20 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 public class TutorialLogic : MonoBehaviour
 {
     public Text tutorialText;
     float timer = 0f;
     public Text timerText;
+    bool text1 = false;
+    bool text2 = false;
+    bool text3 = false;
+    bool text4 = false;
+    bool text5 = false;
+    bool text6 = false;
+    bool text7 = false;
 
     // Start is called before the first frame update
     void Start()
@@ -20,5 +28,45 @@ public class TutorialLogic : MonoBehaviour
     {
         timer += Time.deltaTime;
         timerText.text = timer.ToString("0");
+
+        if (timer >= 5)
+        {
+            tutorialText.text = "Along the way, colourful platforms will appear and you can earn points by matching your colour with the platforms!";
+            text1 = true;
+        }
+        if (timer >= 10 && text1)
+        {
+            tutorialText.text = "Press SPACE to jump. Cmon, let's see you bounce!";
+            text2 = true;
+        }
+        if (timer >= 15 && text2)
+        {
+            tutorialText.text = "To change colours, use the following keys!";
+            text3 = true;
+        }
+        if (timer >= 18 && text3)
+        {
+            tutorialText.text = "Press I for RED!";
+            text4 = true;
+        }
+        if (timer >= 21 && text4)
+        {
+            tutorialText.text = "Press O for BLUE!";
+            text5 = true;
+        }
+        if (timer >= 24 && text5)
+        {
+            tutorialText.text = "Press P for YELLOW!";
+            text6 = true;
+        }
+        if (timer >= 27 && text6)
+        {
+            tutorialText.text = "Nice! Hope you got the hang of it cause your adventure begins now!";
+            text7 = true;
+        }
+        if (timer >= 32 && text7)
+        {
+            SceneManager.LoadScene("Game");
+        }
     }
 }
