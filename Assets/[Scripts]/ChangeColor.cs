caf9d80c965b767194bc2170a8241396f637632a
Jonathan Narine <jonathan.narine@ontariotechu.net>
2021-11-25 17:02:15 -0500

    Added Color DLL

---
+++ Assets/[Scripts]/ChangeColor.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Runtime.InteropServices;
+using UnityEngine;
+
+
+
+public class ChangeColor : MonoBehaviour
+{
+
+    [DllImport("ColorSwitch")]
+
+	private static extern float incrementFloatValue(float value, float increment, float max);
+
+    public float increment;
+
+	private Material currentMaterial;
+
+    private float H, S, V;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        currentMaterial = GetComponent<Renderer>().material;
+        Invoke("changeColor", 0.1f);
+
+    }
+
+    void changeColor() 
+
+        {
+
+        Color.RGBToHSV(currentMaterial.color, out H, out S, out V);
+
+        currentMaterial.color = Color.HSVToRGB(incrementFloatValue(H, 0.01f, 1.0f), S, V);
+
+        Invoke("changeColor", 0.1f);
+
+        }
+}
